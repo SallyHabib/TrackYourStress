8b98666b0f747c2cfa4c0a17d9977e8fdcf5ab80
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-07-12 14:26:16 +0200

    tipsdetails

---
+++ src/app/models/tipsDetails.ts
@@ -0,0 +1,14 @@
+export class TipDetails {
+    constructor(
+      // general
+      public id: number,
+  
+      // attributes
+      public title: string,
+      public text: string,
+      public goal: string,
+      //links
+      public explanation: string
+    ) { }
+  }
+  
