760cc4ad9618dda8118a6df34428b0fca9782aeb
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-07-25 15:42:17 +0200

    chart

--- src/app/q03-s02/q03-S02.component.ts
+++ src/app/q03-s02/q03-S02.component.ts
@@ -12,9 +12,8 @@ import { Chart } from 'chart.js';
 })
 export class q03S02 implements OnInit {
     answersQ02:answers[]=[]
-    values=[]
     chart=false
-
+    //temp_max:number[]=[]
     constructor(
         private myapiService: MyApiService,
         private router: Router,
@@ -28,58 +27,23 @@ export class q03S02 implements OnInit {
     ngOnInit(){
 
         this.gettingApiData()
-        this.chartView()
+       // this.chartView()
     }
-    chartView(){
-    //    var ctx = document.getElementById("canvas");
-    //    console.log(ctx)
-       let ctx = this.elementRef.nativeElement.querySelector(`canvas`);
-       
-     if(ctx){
-     if(ctx instanceof HTMLCanvasElement){
-         console.log("D")
-      // var myC=new Chart(ctx,{});
-        var myChart= new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: ["date","value","frfrfrfr","jjj"],
-            datasets: [
-              { 
-                data:this.values,
-                borderColor: "#3cba9f",
-                fill: false
-              },
-              { 
-                data:[12,13,14,15] ,
-                borderColor: "#ffcc00",
-                fill: false
-              },
-            ]
-          },
-          options: {
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [{
-                display: true
-              }],
-              yAxes: [{
-                display: true
-              }],
-            }
-          }
-        });
-      }
-}
-    }
+    // chartView(){
+    //   console.log(this.values)
+    // //    var ctx = document.getElementById("canvas");
+    // //    console.log(ctx)
+      
+    // }
   gettingApiData(){
     const req = this.myapiService.getAnswerQuesstionaire3Question2();
     req.subscribe( resp => {
         const answersData = resp.body['data'];
+        let temp_max = resp.body['data']['attributes']['answers'].map(resp => resp.value);
+        //console.log(temp_max)
         //console.log(answersData)
         let answersArray=answersData['attributes']['answers'];
-        //console.log(answersArray)
+       // console.log(answersArray.length)
         for(const answerData of answersArray){
             //console.log(answerData['label'])
             this.answersQ02.push(
@@ -88,11 +52,48 @@ export class q03S02 implements OnInit {
                     answerData['value']
                 )
             )
-            this.values.push(answerData['value'])
         }
-        console.log(this.values)
+        
+        console.log(temp_max.length)
 
-
+    
+        let ctx = this.elementRef.nativeElement.querySelector(`canvas`);
+           
+        if(ctx){
+        if(ctx instanceof HTMLCanvasElement){
+            console.log("D")
+         // var myC=new Chart(ctx,{});
+           var myChart= new Chart(ctx, {
+             type: 'line',
+             data: {
+               labels: ["date","value","frfrfrfr","jjj"],
+               datasets: [
+                 { 
+                   data:temp_max,
+                   borderColor: "#3cba9f",
+                   fill: false
+                 }
+               ]
+               
+             },
+             options: {
+               legend: {
+                 display: false
+               },
+               scales: {
+                 xAxes: [{
+                   display: true
+                 }],
+                 yAxes: [{
+                   display: true
+                 }],
+               }
+             }
+           });
+         }
+    }
     });
+
+    
   }  
 }
