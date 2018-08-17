e1185e0f9766143eb224b7ec0bc4cd990e85ce44
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-08-17 13:13:45 +0200

    translation

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -6,8 +6,6 @@ import { AlertService } from '../services/alert.service';
 import { Subject } from 'rxjs/Subject';
 import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
 import {ISubscription} from 'rxjs/Subscription';
-import { Chart } from 'chart.js';
-import { FlashMessagesService, FlashMessagesModule } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-achievements',
@@ -26,8 +24,6 @@ export class achievementsComponent implements OnInit {
         private tokenrefresher: TokenrefresherService,
         private alertService: AlertService,
         private translate: TranslateService,
-        private flashMessage:FlashMessagesModule,
-        private _flashMessagesService: FlashMessagesService
 
 
       ) {
@@ -45,14 +41,18 @@ export class achievementsComponent implements OnInit {
        _close() {
         var mod = document.getElementById("di");
         mod.style.display = "none";
-        this.alertService.enterMessage("You have't unlocked achievements ",1);
+        this.translate.get('ErrorUnlockAchievements').subscribe((res: string) => {
+          this.alertService.enterMessage(res, 1);
+        });
         this.router.navigate(["/home"])
         
       }
 
       success() {
        
-        this.alertService.enterMessage("You have unlocked achievements ",0);
+        this.translate.get('SuccessUnlockAchievements').subscribe((res: string) => {
+          this.alertService.enterMessage(res, 0);
+        });
         this.router.navigate(["/member/myquestionnaires"])
         
       }
